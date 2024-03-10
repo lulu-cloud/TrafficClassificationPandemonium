fad601c3d372f5f93d8f7708349425ce2c1e9037
lulu-cloud <xulu_lili@163.com>
2024-03-10 11:35:48 +0800

    fix: 对基于flowcontainer预处理代码的更新

--- entry/pcap2npy/1_preprocess_with_flowcontainer.py
+++ entry/pcap2npy/1_preprocess_with_flowcontainer.py
@@ -14,7 +14,7 @@ from preprocess.process_pcap_with_flowcontainer import getPcapIPLength
 from utils.split_numpy_data import split_data
 
 def main():
-    yaml_path = r"../configuration/traffic_classification_configuration.yaml"
+    yaml_path = r"../../configuration/traffic_classification_configuration.yaml"
     cfg = setup_config(yaml_path) # 获取 config 文件
     pay, seq, label = getPcapIPLength(
         cfg.preprocess.traffic_path,
@@ -22,7 +22,7 @@ def main():
         cfg.preprocess.ip_length,
         cfg.preprocess.packet_num,
         cfg.preprocess.byte_num)
-    split_data(pay,seq,label,cfg.preprocess.train_size,cfg.preprocess.datasets)
+    split_data(pay,seq,label,cfg.preprocess.train_size,cfg.preprocess.flowcontainer.datasets)
 
 if __name__=="__main__":
     main()
